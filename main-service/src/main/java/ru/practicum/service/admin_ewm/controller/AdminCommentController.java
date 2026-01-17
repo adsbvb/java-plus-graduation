27fc13058d8c0855f1534d0c338fc99b84cfcc47
adsbvb <adsbvb@yandex.ru>
2026-01-17 13:39:40 +0500

    fix: admin comment api

--- main-service/src/main/java/ru/practicum/service/admin_ewm/controller/AdminCommentController.java
+++ main-service/src/main/java/ru/practicum/service/admin_ewm/controller/AdminCommentController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.service.admin_ewm.service.AdminCommentService;
@@ -42,6 +43,7 @@ public class AdminCommentController {
     }
 
     @DeleteMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(
             @PathVariable(name = "commentId") @Positive Long commentId) {
         log.info("DELETE /admin/comment/{}", commentId);
