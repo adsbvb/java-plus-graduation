1f8c32a5eaa6b5121b3edfc2da31a261b4800d8d
adsbvb <adsbvb@yandex.ru>
2026-04-23 18:09:59 +0500

    feat: изменить структуру проекта, настроить - Spring Cloud Eureka, Spring Cloud Config Server, Spring Cloud Gateway

--- infra/gateway-server/src/main/java/ru/practicum/GatewayServerApp.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayServerApp.java
@@ -1,4 +1,4 @@
-package ru.practicum.stats;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StatsApp {
+public class GatewayServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(StatsApp.class, args);
+        SpringApplication.run(GatewayServerApp.class, args);
     }
 }
